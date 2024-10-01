9b410db5d60cbb1b59767a27d26fb7a818c11658
CyanidePineapple <joshuabeecher2003@gmail.com>
2024-10-01 16:31:50 -0400

    Halfway Done

--- server.js
+++ server.js
@@ -9,10 +9,24 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+const Fruit = require("./models/fruit.js");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
+
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+app.post("/fruits", async (req, res) => {
+  console.log(req.body);
+  res.redirect("/fruits/new");
+});
+
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
